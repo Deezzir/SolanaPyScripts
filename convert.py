b89097cad34c6ae8603f2e6fe13d8ea88e869020
Iurii Kondrakov <deezzir@gmail.com>
2024-07-04 13:56:40 -0800

    Update convert.py

--- convert.py
+++ convert.py
@@ -2,6 +2,9 @@ import base58
 from solders.keypair import Keypair
 import json
 import os
+import csv
+
+TARGET_FOLDER = "./keys"
 
 
 def convert_private_key_to_file(base58_private_key, file_path):
@@ -12,9 +15,7 @@ def convert_private_key_to_file(base58_private_key, file_path):
     print(f"Private key saved to {file_path}")
 
 
-def process_private_keys_file(input_file):
-    os.makedirs("keys", exist_ok=True)
-
+def process_keys_txt(input_file):
     with open(input_file, "r") as f:
         private_keys = f.readlines()
 
@@ -26,6 +27,27 @@ def process_private_keys_file(input_file):
         convert_private_key_to_file(base58_private_key, file_path)
 
 
+def process_keys_csv(input_file):
+    with open(input_file, "r") as f:
+        private_keys = csv.reader(f, delimiter=",")
+
+        next(private_keys)  # Skip header
+
+        for idx, row in enumerate(private_keys, start=1):
+            base58_private_key = row[1]
+            file_path = f"./keys/key{idx}.json"
+            convert_private_key_to_file(base58_private_key, file_path)
+
+
 if __name__ == "__main__":
-    input_file = "./private.txt"  # Replace with your input file path
-    process_private_keys_file(input_file)
+    input_file = "./private.txt"
+
+    os.makedirs(TARGET_FOLDER, exist_ok=True)
+    _, ext = os.path.splitext(input_file)
+    if ext == ".txt":
+        process_keys_txt(input_file)
+    elif ext == ".csv":
+        process_keys_csv(input_file)
+    else:
+        print("Unsupported file format")
+        exit(1)
