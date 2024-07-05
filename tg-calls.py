4834c27615d3bec22bc442f9ecc4daeb3fe7b822
Iurii Kondrakov <deezzir@gmail.com>
2024-07-05 09:01:26 -0700

    Updates

--- tg-calls.py
+++ tg-calls.py
@@ -7,12 +7,14 @@ import asyncio
 from pytgcalls import idle  # type: ignore
 from pytgcalls import PyTgCalls  # type: ignore
 from pytgcalls.types import MediaStream  # type: ignore
-from telethon.sync import TelegramClient  # type: ignore
+from telethon.sync import TelegramClient, errors  # type: ignore
 from telethon.sessions import StringSession  # type: ignore
+from telethon.tl.functions.messages import ImportChatInviteRequest  # type: ignore
 
 PHONES_CSV: str = "./assets/phones.csv"
 SESSIONS_CSV: str = "./assets/sessions.csv"
-GROUP_ID: int = -100123456789  # replace with your group id
+GROUP_ID: int = -1002193610260
+INVITE_LINK: str = "https://t.me/+pX_jSF5o3FAwZGIx"
 
 
 def read_phones_csv(file_path: str) -> Optional[pd.DataFrame]:
@@ -24,6 +26,19 @@ def read_phones_csv(file_path: str) -> Optional[pd.DataFrame]:
             dtype={0: str, 1: str, 2: str, 3: str},
             names=["phone", "password", "api_id", "api_hash"],
         )
+
+        if csv_file.shape[0] == 0:
+            print(f"CSV file '{file_path}' is empty.")
+            return None
+
+        if csv_file.isnull().values.any():
+            print(
+                f"CSV file '{file_path}' contains missing values. Structure: Phone, Password, API ID, API Hash."
+            )
+            return None
+
+        print(f"Read {csv_file.shape[0]} phone entries")
+
         return csv_file
     except FileNotFoundError as e:
         print(f"File '{file_path}' not found: {e}")
@@ -66,25 +81,46 @@ def create_sessions_csv(file_path: str, phones_df: pd.DataFrame) -> None:
     sessions_df.to_csv(file_path, index=False, header=False)
 
 
-def join_tg_call(
-    phone: str, session: str, api_id: str, api_hash: str
-) -> Optional[PyTgCalls]:
+async def join_tg_call(tg_client: TelegramClient, phone: str) -> Optional[PyTgCalls]:
     print(f"Joining call with phone: {phone}")
-    tg_client = TelegramClient(StringSession(session), api_id, api_hash)
     call_client = PyTgCalls(tg_client)
 
-    # sleep random time to avoid flood wait
-    # time.sleep(random.randint(1, 5))
-
     try:
-        call_client.start()
-        call_client.play(GROUP_ID, None)
-        call_client.mute_stream(GROUP_ID)
+        await call_client.start()
+        await call_client.play(GROUP_ID, None)
+        await call_client.mute_stream(GROUP_ID)
     except Exception as e:
         print(f"Error joining call: {e}")
         return None
 
     return call_client
+
+
+async def join_tg_group(
+    tg_client: TelegramClient, invite_link: str, phone: str
+) -> None:
+    print(f"Joining group with phone: {phone}")
+
+    try:
+        chat = await tg_client.get_entity(invite_link)
+        print(f"Already in the chat: {chat.title}")
+    except errors.UserAlreadyParticipantError:
+        print("You are already a participant of this chat.")
+    except errors.InviteHashInvalidError:
+        print("The invite link is invalid.")
+    except errors.InviteHashExpiredError:
+        print("The invite link has expired.")
+    except Exception as e:
+        try:
+            hash = invite_link.split("/")[-1].replace("+", "")
+            await tg_client(ImportChatInviteRequest(hash))
+            print("Successfully joined the group.")
+        except errors.InviteHashInvalidError:
+            print("The invite link is invalid.")
+        except errors.InviteHashExpiredError:
+            print("The invite link has expired.")
+        except Exception as e:
+            print(f"Failed to join the group: {e}")
 
 
 def process_sessions_csv(file_path: str) -> None:
@@ -97,20 +133,32 @@ def process_sessions_csv(file_path: str) -> None:
     )
 
     clients = []
-    for _, session_entry in sessions_df.iterrows():
-        client = join_tg_call(
-            session_entry["phone"],
-            session_entry["session"],
-            session_entry["api_id"],
-            session_entry["api_hash"],
-        )
-        if client is not None:
-            clients.append(client)
+    loop = asyncio.get_event_loop()
+
+    async def process_entries():
+        for _, session_entry in sessions_df.iterrows():
+            phone = session_entry["phone"]
+            print(f"Processing session for phone: {phone}")
+            tg_client = TelegramClient(
+                StringSession(session_entry["session"]),
+                session_entry["api_id"],
+                session_entry["api_hash"],
+            )
+            await tg_client.start()
+            await join_tg_group(tg_client, INVITE_LINK, phone)
+            client = await join_tg_call(tg_client, phone)
+            if client is not None:
+                clients.append(client)
+
+    loop.run_until_complete(process_entries())
 
     idle()
 
     for client in clients:
-        client.leave_call(GROUP_ID)
+        try:
+            client.leave_call(GROUP_ID)
+        except Exception as e:
+            print(f"Error leaving call: {e}")
     print("All calls done")
 
 
